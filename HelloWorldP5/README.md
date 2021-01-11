d5d623103b131850a6dcfccf1cfd203e18ffca2f
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2021-01-11 13:48:49 -0800

    added HelloWorld

---
+++ HelloWorldP5/README.md
@@ -0,0 +1,11 @@
+## Hello World for P5.js
+#### by Scott Kildall
+January 6th, 2020
+
+
+### Overview
+Simple Hello World text displau.
+
+### Technical Details
+
+Modify the sketch.js setup() and draw() are used only
---
+++ HelloWorldP5/sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+  fill(240,56,0);
+  text("Hello, World", width/2, height/2);
+}
