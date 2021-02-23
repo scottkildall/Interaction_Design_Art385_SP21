22f73d30b1d2b89d856fe15bc6ba66e28c1e7a12
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2021-02-22 17:21:03 -0800

    Added SimplePNG Clickable example

---
+++ Clickable Examples/README.md
@@ -0,0 +1,18 @@
+# Clickable Examples
+#### by Scott Kildall
+www.kildall.com
+
+
+## Overview
+These are some samples for the p5.clickable library (https://github.com/Lartu/p5.clickable)
+
+## Things I have noticed
+(1) For the Clickable class, it will always draw and do mouseX and mouseY calculations from the upper-left corner, so you need to make sure that you are in imageMode(CORNER) and rectMode(CORNER), otherwise it won't interact correctly
+
+(2) If you want background transparent PNGs, set the .color field of the clickable object to be "#00000000"
+
+
+## Examples
+
+(1) SimplePNGButton: Displays a single PNG button with a cat image, alert to show it was clicked. Uses the onHover and onOutside functions to tint the image
+
---
+++ Clickable Examples/SimplePNGButton/sketch.js
@@ -0,0 +1,80 @@
+
+/***********************************************************************************
+  SimplePNGButton
+  by Scott Kildall
+  
+  Start your localhost before running this, otherwise no PNGs will display
+
+  Shows an example of a PNG button using the Clickable class
+
+***********************************************************************************/
+
+
+// A Clickable object
+var catButton;
+
+// Image loaded from the assets folder
+var catImage;
+
+function preload(){
+  catImage = loadImage('assets/cat.png');
+}
+function setup() {
+  createCanvas(600,400);
+
+  makeCatButton();
+}
+
+// Just draw the button
+function draw() {
+  background(128);
+  catButton.draw();
+}
+
+function makeCatButton() {
+
+  // Create the clickable object
+  catButton = new Clickable();
+  
+  // set the image to be this PNG
+  catButton.image = catImage;
+  catButton.text = "";
+
+  // This should set the color to be off OR background transparent
+  //catButton.color = "#00000000";
+
+  // This would give it a white background
+  catButton.color = "#FFFFFF";
+
+  // set width + height to image size
+  catButton.width = catImage.width;
+  catButton.height = catImage.height;
+
+  // set to middle of screen, since we are drawing from the corners, we need to make an
+  // additional calculation here
+  catButton.locate( width/2 - catButton.width/2 , height/2 - catButton.height/2 );
+
+  // Clickable callback functions, defined below
+  catButton.onPress = catButtonPressed;
+  catButton.onHover = catButtonHover;
+  catButton.onOutside = catButtonOnOutside;
+}
+
+// Meow alert box when pressed (mouse down)
+catButtonPressed = function () {
+  alert("meow");
+}
+
+// tint when mouse is over
+catButtonHover = function () {
+  this.tint = "#FF4411";
+  this.noTint = false;
+}
+
+// return to normal when it is outside
+catButtonOnOutside = function () {
+  this.noTint = true;
+}
+
+
+ 
